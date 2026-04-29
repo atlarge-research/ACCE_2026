e8f9c710fb8834eea4ed8cf47b97fc6e13e625eb
bokke1010 <bokke1010@gmail.com>
2026-04-29 15:31:48 +0200

    add 4 and 5

---
+++ Labs/Lab4/README.md
@@ -0,0 +1,3 @@
+# hello world HLS project: vector addition
+
+`sbatch job.sh` to run the tcl script on DAS5
---
+++ Labs/Lab4/hello_hls.h
@@ -0,0 +1,7 @@
+#pragma once  // Prevent multiple inclusions
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototype
+void add(int *a, int *b, int *c, int size);
